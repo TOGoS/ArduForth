e6519ea438839cb5fd910b8b10c7575a031a5541
Dan Stevens <stevens@harold.(none)>
2013-06-25 16:00:34 -0500

    Working on forth interpreter.

---
+++ Forth/Forth.ino
@@ -0,0 +1,60 @@
+const byte ArduForth_MAX_TOKEN_LENGTH = 32;
+
+char ArduForth_tokenBuffer[ArduForth_MAX_TOKEN_LENGTH];
+byte ArduForth_tokenBufferLength = 0;
+
+void ArduForth_flushToken() {
+  ArduForth_tokenBuffer[ArduForth_tokenBufferLength++] = 0;
+  Serial.print("You said: ");
+  Serial.println(ArduForth_tokenBuffer);
+  ArduForth_tokenBufferLength = 0;
+}
+
+void ArduForth_handleChar( char c ) {
+  // TODO: Handle double-quoted strings
+  switch( c ) {
+  case '\n': case ' ': case '\t': case '\r':
+    ArduForth_flushToken();
+    break;
+  default:
+    ArduForth_tokenBuffer[ArduForth_tokenBufferLength++] = c;
+  }
+  if( ArduForth_tokenBufferLength >= ArduForth_MAX_TOKEN_LENGTH ) {
+    Serial.println("Token too long.");
+  }
+}
+
+typedef unsigned long int millis_t;
+
+void setup() {
+  Serial.begin(9600);
+  while( !Serial ) { }
+  Serial.println();
+  Serial.println("Hello, world!");
+}
+
+millis_t nextTickTime = 0;
+void loop() {
+  millis_t time = millis();
+  while( Serial.available() > 0 ) {
+    ArduForth_handleChar(Serial.read());
+  }
+  
+  if( time - nextTickTime >= 0 ) {
+    if( Serial ) {
+      Serial.print("Tick! Target: ");
+      Serial.print(nextTickTime);
+      Serial.print(", actual:");
+      Serial.print(time);
+      Serial.println();
+    }
+    nextTickTime += 1000;
+    // Compare by substraction to avoid weird behavior
+    // near overflow
+    if( time - nextTickTime > 0 ) {
+      // Still behind?
+      nextTickTime = time + 1000;
+    }
+  }
+}
+
